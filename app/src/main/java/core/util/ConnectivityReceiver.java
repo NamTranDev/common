ae16c41d0f39c265eff75b3cf2c4e44fb73885e8
Le Duc Tuan <leductuan.mail@gmail.com>
2016-09-27 14:28:31 +0700

    Add enable and disable connectivity receiver programmatically

--- app/src/main/java/core/util/ConnectivityReceiver.java
+++ app/src/main/java/core/util/ConnectivityReceiver.java
@@ -1,8 +1,10 @@
 package core.util;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
@@ -20,8 +22,23 @@ public class ConnectivityReceiver extends BroadcastReceiver {
     private static boolean connected = true;
     private static boolean disconnected = true;
 
+    public static void enable() {
+        if (BaseApplication.getContext() != null) {
+            ComponentName receiver = new ComponentName(BaseApplication.getContext(), ConnectivityReceiver.class);
+            PackageManager pm = BaseApplication.getContext().getPackageManager();
+            pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+        }
+    }
+
+    public static void disable() {
+        if (BaseApplication.getContext() != null) {
+            PackageManager pm = BaseApplication.getContext().getPackageManager();
+            pm.setComponentEnabledSetting(new ComponentName(BaseApplication.getContext(), ConnectivityReceiver.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        }
+    }
+
     public static void registerListener(ConnectivityListener listener) {
-        if (listener != null) {
+        if (listener != null && !listeners.containsKey(listener)) {
             listeners.put(listener, listener);
         }
     }
